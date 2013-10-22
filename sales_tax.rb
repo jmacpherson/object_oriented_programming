711cae4ada1ac82e41d88ab2cadff5b307cb40ba
jmacpherson <jd.macpherson@gmail.com>
2013-10-22 17:58:07 -0400

    fixed integer-float conversion in calc_tax, fixed display of floats in receipt

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,3 +1,5 @@
+# Classes
+
 class CashRegister
   @@sales_tax = 10
   @@import_duty = 5
@@ -5,12 +7,19 @@ class CashRegister
   def calc_tax_rate(item)
     tax_rate = 0
     tax_rate += @@import_duty if item.imported
-    tax_rate += @@sales_tax unless (item.class == Book) || (item.class == Food) || (item.class == Medicine)
+    tax_rate += @@sales_tax unless (item.is_a? Book) || (item.is_a? Food) || (item.is_a? Medicine)
   end
 
   def calc_tax(item)
     tax = 0
-    tax += ((item.price * item.number) * calc_tax_rate(item)) / 100
+    tax += (((item.price * item.number.to_f) * calc_tax_rate(item).to_f) / 100)
+
+    #round to nearest 0.05
+    if tax % 0.05 != 0
+      tax += 0.05 - (tax % 0.05)
+    end
+    
+    tax
   end
 
   def total(item)
@@ -18,18 +27,30 @@ class CashRegister
   end
 
   def receipt(*items)
-    applied_tax = items.inject(0) { |sum,item| sum += calc_tax(item) }
-    receipt_total = items.inject(0) { |sum,item| sum =+ total(item) }
+    #calculate tax on each item
+    items_tax = items.collect { |item| calc_tax(item) }
 
+    #calculate total tax for receipt
+    applied_tax = 0
+    items_tax.each { |tax| applied_tax += tax }
+    # applied_tax = sprintf("%.2f", applied_tax)
+
+    #calculate total price for receipt
+    receipt_total = 0
+    items.each { |item| receipt_total += total(item) }
+
+    #return receipt printout
     receipt_printout = String.new
-    receipt_printout << items.each { |item| "#{item.number} #{item.name}: #{item.price} " } << "Sales Tax: #{applied_tax} Total: #{receipt_total}"
+    items.each { |item| receipt_printout << "#{item.number} #{item.name}: #{item.price} " }
+    receipt_printout << "Sales Tax: #{sprintf("%.2f",applied_tax)} Total: #{sprintf("%.2f",receipt_total)}"
   end
 end
 
 class Product
-  attr_reader :price, :number, :imported
+  attr_reader :name, :price, :number, :imported
 
-  def initialize (price, number=1, imported=false)
+  def initialize (name, price, imported=false, number=1)
+    @name = name
     @price = price
     @number = number
     @imported = imported
@@ -44,3 +65,15 @@ end
 
 class Medicine < Product
 end
+
+# Tests
+
+register = CashRegister.new
+
+book = Book.new("book",12.49)
+cd = Product.new("music CD",14.99)
+choc_bar = Food.new("chocolate bar",0.85)
+
+puts register.calc_tax(cd)
+
+puts register.receipt(book, cd, choc_bar)
